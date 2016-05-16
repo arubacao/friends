f97a66269dfb01e36314fae5d1b632618341a5af
arubacao <arubacao@gmail.com>
2016-05-15 20:34:33 -0400

    Applied fixes from StyleCI

--- src/Traits/Friendable.php
+++ src/Traits/Friendable.php
@@ -6,7 +6,6 @@
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
- *
  */
 namespace Arubacao\Friendships\Traits;
 
@@ -39,11 +38,11 @@ trait Friendable
         $previouslyDeniedFriendship = $this->findFriendshipsWith($recipient)
             ->whereStatus(Status::DENIED)
             ->whereSender($recipient);
-        if($previouslyDeniedFriendship->exists())
-        {
+        if ($previouslyDeniedFriendship->exists()) {
             $previouslyDeniedFriendship->update([
-                'status' => Status::ACCEPTED
+                'status' => Status::ACCEPTED,
             ]);
+
             return $previouslyDeniedFriendship;
         }
 
