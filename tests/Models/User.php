ebb8cdd954f35f69fa96dace2afd1e2fb464fa10
arubacao <arubacao@gmail.com>
2016-06-18 22:12:28 +0200

    add user

--- tests/Models/User.php
+++ tests/Models/User.php
@@ -8,25 +8,6 @@
  * file that was distributed with this source code.
  */
 
-
-/**
- * This file is part of Friends.
- *
- * (c) Christopher Lass <arubacao@gmail.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
-**
- * This file is part of Friends.
- *
- * (c) Christopher Lass <arubacao@gmail.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 namespace Arubacao\Tests\Friends\Models;
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
